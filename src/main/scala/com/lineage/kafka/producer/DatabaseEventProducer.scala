9af687371c0351410559090b03082c3972d18c05
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-12-02 18:28:14 -0300

    fix: producers config

--- src/main/scala/com/lineage/kafka/producer/DatabaseEventProducer.scala
+++ src/main/scala/com/lineage/kafka/producer/DatabaseEventProducer.scala
@@ -14,7 +14,7 @@ object DatabaseEventProducer {
     val props = new Properties()
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer")
     
     val producer = new KafkaProducer[String, Array[Byte]](props)
     val serde = new DataEventSerde()
